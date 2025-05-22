d2c244bf7bfb222edc430d38320fe7da66a687c4
Rajshree <rajshreeakm@gmail.com>
2025-05-23 00:22:29 +0530

    Created Educator's routes

--- virtualearn/src/App.jsx
+++ virtualearn/src/App.jsx
@@ -7,10 +7,18 @@ import MyEnrollments from './pages/student/MyEnrollments'
 import Player from './pages/student/Player'
 import Loading from './components/student/Loading'
 
+import Educator from './pages/educator/Educator'
+import Dashboard from './pages/educator/Dashboard'
+import AddCourse from './pages/educator/AddCourse'
+import MyCourses from './pages/educator/MyCourses'
+import StudentsEnrolled from './pages/educator/StudentsEnrolled'
+
 const App = () => {
   return (
     <div>
       <Routes>
+
+        {/* Student's Routes  */}
         <Route path='/' element={<Home/>}/>
         <Route path='/course-list' element={<CoursesList/>}/>
         <Route path='/course-list/:input' element={<CoursesList/>}/>
@@ -18,6 +26,14 @@ const App = () => {
         <Route path='/my-enrollments' element={<MyEnrollments/>}/>
         <Route path='/player/:courseId' element={<Player/>}/>
         <Route path='/loading/:path' element={<Loading/>}/>
+
+        {/* Educator's Routes */}
+        <Route path='/educator' element={<Educator/>}>
+        <Route path='educator' element={<Dashboard/>}/>
+        <Route path="add-course" element={<AddCourse />} /> 
+        <Route path='my-courses' element={<MyCourses/>}/>
+        <Route path='students-enrolled' element={<StudentsEnrolled/>}/>
+        </Route>
       </Routes>
     </div>
   )
--- virtualearn/src/pages/educator/Educator.jsx
+++ virtualearn/src/pages/educator/Educator.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import { Outlet } from 'react-router-dom'
 const Educator = () => {
   return (
     <div>
        <h1>Educator Page</h1>
+       <div>
+        {<Outlet/>}
+       </div>
     </div>
   )
 }
