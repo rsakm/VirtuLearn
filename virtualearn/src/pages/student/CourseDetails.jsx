feed5118ab63f2abaf2de8dd52a449ebfebb5613
Rajshree <rajshreeakm@gmail.com>
2025-06-14 22:47:00 +0530

    Implemented the toggle feature in the course structure drop down menu to colaps and expand, also implemnted the rotation of the arrow accordingly

--- virtualearn/src/pages/student/CourseDetails.jsx
+++ virtualearn/src/pages/student/CourseDetails.jsx
@@ -10,6 +10,7 @@ const CourseDetails = () => {
 
   const {id} = useParams();
   const [courseData, setCourseData] = useState(null);
+  const [openSections, setOpenSections] = useState({});
 
   const {allCourses, calculateRating, calculateCHapterTime, calculateCourseDuration, calculateNoOfLectures} = useContext(AppContext);
 
@@ -21,6 +22,13 @@ const CourseDetails = () => {
   useEffect(()=>{
     fetchCourseData();
   },[]);
+
+  const toggleSection = (index)=>{
+    setOpenSections((prevOpenSections) => ({
+      ...prevOpenSections,
+      [index]: !prevOpenSections[index],
+    }));
+  }
 
   return courseData? (
   <>
@@ -61,15 +69,15 @@ const CourseDetails = () => {
             <div className='pt-5'>
               {courseData.courseContent.map((chapter, index)=>(
                 <div key={index} className='border border-gray-300 bg-white mb-2 rounded'>
-                  <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none'>
+                  <div className='flex items-center justify-between px-4 py-3 cursor-pointer select-none' onClick={() => toggleSection(index)}>
                     <div className='flex items-center gap-2'>
-                      <img src={assets.down_arrow_icon} alt="arrow icon" />
+                      <img className={`transform transition-transform duration-300 ${openSections[index] ? 'rotate-180' : 'rotate-0'}`} src={assets.down_arrow_icon} alt="arrow icon" />
                       <p className='font-medium md:text-base text-sm'>{chapter.chapterTitle}</p>
                     </div>
                     <p className='text-sm md:text-default'>{chapter.chapterContent.length} lectures - {calculateCHapterTime(chapter)}</p>
                   </div>
 
-                  <div className='overflow-hidden transition-all duration-300 max-h-96'>
+                  <div className={`overflow-hidden transition-all duration-300  ${openSections[index] ? 'max-h-96' : 'max-h-0'}`}>
                     <ul className='list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300'>
                       {chapter.chapterContent.map((lecture,index)=>(
                         <li key={index} className='flex items-start gap-2 py-1'>
