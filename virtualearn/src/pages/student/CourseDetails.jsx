47390a58d96c0bfa95f15f00c661062fd0d3240e
Rajshree <rajshreeakm@gmail.com>
2025-06-16 20:58:23 +0530

    Implementing the right column of the courseDetails  page

--- virtualearn/src/pages/student/CourseDetails.jsx
+++ virtualearn/src/pages/student/CourseDetails.jsx
@@ -110,9 +110,14 @@ const CourseDetails = () => {
         </div>
 
         {/* Right column */}
-        <div>
-          
-
+        <div className='max-w-course-card z-10 shadow-custom-card rounded-t md:rounded-none overflow-hidden bg-white min-w-[300px] sm:min-w-[420px]'>
+          <img src={courseData.courseThumbnail} alt="course thumbnail" />
+          <div className='pt-5'>
+            <div>
+              <img className='w-3.5' src={assets.time_left_clock_icon} alt="time left clock icon" />
+              <p className='text-red-500'><span className='font-medium'>5 days</span> left at this price</p>
+            </div>
+          </div>
         </div>
       
     </div>
